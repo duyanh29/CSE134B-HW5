aa4441d2028d760e8bed895d447b959b684041ae
duyanh29 <77292869+duyanh29@users.noreply.github.com>
2023-03-15 20:45:19 -0700

    Add files via upload

--- ButtonCount.js
+++ ButtonCount.js
@@ -3,15 +3,17 @@ class ButtonCount extends HTMLElement{
         super();
         this.countNum = 0;
         this.attachShadow({mode: 'open'});
+
+
         let buttonCount_Template = document.createElement('template');
         buttonCount_Template.innerHTML = `<button id="countBtn"></button>`;
-        this.shadowRoot.appendChild(buttonCount_Template.content.cloneNode(true));
+        let tempBtn = buttonCount_Template.content.cloneNode(true);
+        this.shadowRoot.appendChild(tempBtn);
         this.shadowRoot.getElementById('countBtn').innerText = "Time clicked: " + this.countNum;
     }
     connectedCallback(){
         this.shadowRoot.getElementById('countBtn').addEventListener('click', () =>{
-            this.countNum++; 
-            this.shadowRoot.getElementById('countBtn').innerText = "Time clicked: " + this.countNum;
+            this.shadowRoot.getElementById('countBtn').innerText = "Time clicked: " + ++this.countNum;
         });
     }
 
